f8ca2fe7e644b052ae76913f3abb8dfcb1f84676
william Rodriguez-Ledesma <popolol.wr.wr@gmail.com>
2022-10-17 21:54:15 -0700

    commit tree e548c6b5cb9ef080299fbb8cf276937c24f0c986

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+ let cards =['Will','mike','cole'];
+function writeCards(cards, event ='surprise'){
+ let sentCards = [];
+  for (let i = 0; i < cards.length; i++){
+  sentCards.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+}
+
+return sentCards;
+}
+ 
+function countDown(startingNumber){
+while (startingNumber > 0){
+  console.log(startingNumber);
+  startingNumber -=1;
+
+} 
+console.log(startingNumber);
+}
